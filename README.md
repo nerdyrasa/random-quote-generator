5aee2f4e2c044b4d6f66564551fe7ffb71dc4c75
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-10-31 15:38:29 -0500

    First pass at the project.

--- README.md
+++ README.md
@@ -16,12 +16,12 @@ Each quote object in the quotes array should have
 	- [ ] A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb.”
 	- [ ] An optional citation property which contains a string identifying where the quote comes from, like a speech or publication. For example, "Famous Anonymous Jokes." If there is no known publication, do not include this property on the object.
 	- [ ] An optional year property which contains a number identifying the date of the quote. For example, 1997. If there is no known date, then do not include this property on the object.
-[ ] Create a function named getRandomQuote which:
+- [ ] Create a function named getRandomQuote which:
 	- [ ] selects a random quote object from the quotes array
 	- [ ] returns the randomly selected quote object
-[ ] Create a function named printQuote which follows these rules:
+- [ ] Create a function named printQuote which follows these rules:
 	- [ ] printQuote calls the getRandomQuote function and stores the returned quote object in a variable
-	- [ ]printQuote constructs a string containing the different properties of the quote object using the following HTML template:
+	- [ ] printQuote constructs a string containing the different properties of the quote object using the following HTML template:
 ```
 <p class="quote"> [quote here] </p>
 <p class="source"> [source here]
--- js/script.js
+++ js/script.js
@@ -1,3 +1,80 @@
+var quotes = [
+  {
+    quote: "No Grit, No Pearl.",
+    source: "Anonymous",
+    citation: null,
+    year: null
+  },
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    source: "Wayne Gretzky",
+    citation: null,
+    year: null
+  }, {
+    quote: "Fall seven times, stand up eight.",
+    source: "Japanese Proverb",
+    citation: null,
+    year: null
+  }, {
+    quote: "Success is not final, failure is not fatal: it is" +
+    " the courage to continue that counts.",
+    source: "Winston Churchill",
+    citation: null,
+    year: null
+  }, {
+    quote: "There are seven days in the week and someday isn’t one of them.",
+    source: "Anonymous",
+    citation: null,
+    year: null
+  }, {
+    quote: "The man who views the world at 50 the same as he did at 20 has wasted 30 years of his life.",
+    source: "Muhammad Ali",
+    citation: null,
+    year: null
+  }
+];
+
+var quoteId = 0;
+
+function getRandomQuote() {
+  // Ultimately I'll need to select a random quote object from the quotes array
+  // For now,just cycle through the quotes
+
+  var quote = quotes[quoteId];
+  quoteId++;
+  if (quoteId === 6) {
+    quoteId = 0;
+  }
+  // Return the randomly selected quote object
+  return quote;
+}
+
+function formatQuote(stuff) {
+
+  var formattedQuote =
+    '<p class="quote">' + stuff.quote + '</p>' +
+    '<p class="source">' + stuff.source +
+      /* <span class="citation"> [citation here] </span>
+       <span class="year"> [year here] </span> */
+    '</p>';
+  return formattedQuote;
+
+}
+
+function printQuote() {
+  // Get a quote
+  var quote = getRandomQuote();
+
+  // format the
+  var formatted = formatQuote(quote);
+
+  // Display the quote
+  document.getElementById('quote-box').innerHTML = formatted;
+
+}
+
+
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
