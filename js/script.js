deb55e19f51cbc3fbe1b9ff59a365dbc5b9f88c5
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-10-31 13:55:11 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
